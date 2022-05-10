8919ecd3c5e719cfa865f97a7692e7154d5174b4
Sebastian Arrubia <sarrubia@gmail.com>
2022-05-10 15:52:37 -0300

    v1.4.1 B

--- packageB/main.go
+++ packageB/main.go
@@ -3,4 +3,4 @@ package packageB
 type Pack struct{}
 
 func (p *Pack) Name() string    { return "PackageB" }
-func (p *Pack) Version() string { return "1.4.0-B" }
+func (p *Pack) Version() string { return "1.4.1-B" }
