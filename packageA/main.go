50727d5c4ce3622d34b7c262c5c3bb18288957fb
Sebastian Arrubia <sarrubia@gmail.com>
2022-05-10 15:20:26 -0300

    v1.4.0

--- packageA/main.go
+++ packageA/main.go
@@ -3,4 +3,4 @@ package packageA
 type Pack struct{}
 
 func (p *Pack) Name() string    { return "PackageA" }
-func (p *Pack) Version() string { return "1.0.0-fakeA" }
+func (p *Pack) Version() string { return "1.4.0-A" }
--- packageC/main.go
+++ packageC/main.go
@@ -3,4 +3,4 @@ package packageC
 type Pack struct{}
 
 func (p *Pack) Name() string    { return "PackageC" }
-func (p *Pack) Version() string { return "1.2.0-C" }
+func (p *Pack) Version() string { return "1.4.0-C" }
