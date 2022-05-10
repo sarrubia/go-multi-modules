16b13b59ed246508bcc3a9b3fe197fca72c0e257
Sebastian Arrubia <sarrubia@gmail.com>
2022-05-10 14:56:13 -0300

    v1.0.1 for B

--- packageA/main.go
+++ packageA/main.go
@@ -3,4 +3,4 @@ package packageA
 type Pack struct{}
 
 func (p *Pack) Name() string    { return "PackageA" }
-func (p *Pack) Version() string { return "1.0.0-A" }
+func (p *Pack) Version() string { return "1.0.0-fakeA" }
--- packageB/main.go
+++ packageB/main.go
@@ -3,4 +3,4 @@ package packageB
 type Pack struct{}
 
 func (p *Pack) Name() string    { return "PackageB" }
-func (p *Pack) Version() string { return "1.0.0-B" }
+func (p *Pack) Version() string { return "1.0.1-B" }
